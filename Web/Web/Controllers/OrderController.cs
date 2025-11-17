c4f81c7f39b656f4b46dc73e4426434bfada42d1
MinhTrii4u <laminhtri197@gmail.com>
2025-11-17 16:27:07 +0700

    Mo ta thay doi

---
+++ Web/Web/Controllers/OrderController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Web;
+
+namespace Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private DBADIDASEntities db = new DBADIDASEntities();
+
+        public ActionResult Index()
+        {
+            var orders = db.OrderProes.OrderByDescending(x => x.DateOrder).ToList();
+            return View(orders);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderPro order = db.OrderProes.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderPro order = db.OrderProes.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IDCus = new SelectList(db.Customers, "IDCus", "NameCus", order.IDCus);
+            return View(order);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,DateOrder,IDCus,AddressDeliverry")] OrderPro order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IDCus = new SelectList(db.Customers, "IDCus", "NameCus", order.IDCus);
+            return View(order);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            OrderPro order = db.OrderProes.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            OrderPro order = db.OrderProes.Find(id);
+            db.OrderDetails.RemoveRange(order.OrderDetails);
+            db.OrderProes.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Web/Web/Controllers/PaymentController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Web;
+
+namespace Web.Controllers
+{
+    public class PaymentController : Controller
+    {
+        DBADIDASEntities db = new DBADIDASEntities();
+
+        public ActionResult Index()
+        {
+            var dummyCart = new List<Product>
+            {
+                new Product { NamePro = "GIÀY ULTRABOOST LIGHT", Price = 5000000, ImagePro = "tải xuống.jpg" },
+                new Product { NamePro = "ÁO THUN 3 SỌC", Price = 800000, ImagePro = "sh" }
+            };
+
+            ViewBag.Cart = dummyCart;
+            ViewBag.Total = dummyCart.Sum(x => x.Price);
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ProcessOrder(OrderPro order)
+        {
+            if (ModelState.IsValid)
+            {
+                order.DateOrder = DateTime.Now;
+                db.OrderProes.Add(order);
+                db.SaveChanges();
+
+                return RedirectToAction("Success", new { id = order.ID });
+            }
+
+            return View("Index", order);
+        }
+
+        public ActionResult Success(int? id)
+        {
+            if (id.HasValue)
+            {
+                var order = db.OrderProes.Find(id);
+                return View(order);
+            }
+
+            return View();
+        }
+    }
+}
