2924d9a6e3cccbdd5a32b81b5e7a6da2545004e9
TheDay0710 <24DH113268@st.huflit.edu.vn>
2025-11-21 21:11:01 +0700

    acc

--- Web/Web/DBADIDAS.Designer.cs
+++ Web/Web/DBADIDAS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\beadidas\Web_Adidas\Web\Web\DBADIDAS.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\Web\Web_Adidas\Web\Web\DBADIDAS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- Web/Web/DBADIDASModel.Designer.cs
+++ Web/Web/DBADIDASModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Web Bán Giày\Web_Adidas\Web\Web\DBADIDASModel.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\Web\Web_Adidas\Web\Web\DBADIDASModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
